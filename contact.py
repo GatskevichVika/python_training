e1fb4e557ed0717962a78ce68bcc24fb6726e102
GatskevichVika <gatskevich.vika@gmail.com>
2021-03-21 17:28:38 +0300

    По аналогии с добавлением группы, сделала добавление контакта. Для наглядности взяла больше параметров из анкеты.

---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, company, mobile, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.company = company
+        self.mobile = mobile
+        self.email = email
---
+++ test_add_contact.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest
+from contact import Contact
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/group.php")
+
+    def login(self, wd, username, password):
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def add_new_contact(self, wd, contact):
+        # add new contact
+        wd.find_element_by_link_text("add new").click()
+        # firstname
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        #middlename
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        # lastname
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        # company
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        # mobile
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        # email
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def return_to_homepage(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.add_new_contact(wd, Contact(firstname="Петр", middlename="Петрович", lastname="Петров", company="Петрострой",
+                             mobile="7777777", email="petr@petr.ru"))
+        self.return_to_homepage(wd)
+        self.logout(wd)
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
