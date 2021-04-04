55a28c2146ab22102e14a8ad1ff71ad5c4eea180
GatskevichVika <gatskevich.vika@gmail.com>
2021-04-04 17:43:50 +0300

    Добавлен отдельный метод для заполнения формы fill_form для групп и контактов.

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,10 +7,8 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
 
-    def create(self, contact):
+    def fill_form(self, contact):
         wd = self.app.wd
-        self.open_contact_page()
-        # fill contact form
         # firstname
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -36,8 +34,14 @@ class ContactHelper:
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys(contact.email)
         # submit contact creation
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        wd.find_element_by_xpath("//*[@type='submit']").click()
         self.return_to_homepage()
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.open_contact_page()
+        # fill contact form
+        self.fill_form(contact)
 
     def delete_first_contact(self):
         wd = self.app.wd
@@ -53,33 +57,7 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         #submit edit
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        # edit firstname
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        # edit middlename
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        # edit lastname
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        # edit company
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        # edit mobile
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        # edit email
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        # save
-        wd.find_element_by_name("update").click()
-        self.return_to_homepage()
+        self.fill_form(contact)
 
     def return_to_homepage(self):
         wd = self.app.wd
