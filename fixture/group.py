baa29cbc7e90172b337027f329b437b1777f490b
GatskevichVika <gatskevich.vika@gmail.com>
2021-04-04 18:43:37 +0300

    Добавлены методы модификации группы. Добавлен новый тест - модификация группы. Из метода заполнения формы убрана часть кода с возвращением. Поправлены методы create и edit.

--- fixture/group.py
+++ fixture/group.py
@@ -12,31 +12,45 @@ class GroupHelper:
         self.open_groups_page()
         wd.find_element_by_name("new").click()
         self.fill_form(group)
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        # open modification form
+        # fill form
+        wd.find_element_by_name("edit").click()
+        self.fill_form(new_group_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
-    def fill_form(self, group):
-        # fill group form
+    def select_first_group(self):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # save changes
-        wd.find_element_by_xpath("//*[@type='submit']").click()
-        self.return_to_groups_page()
+        wd.find_element_by_name("selected[]").click()
+
+    def fill_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def edit(self, group):
         wd = self.app.wd
@@ -46,6 +60,8 @@ class GroupHelper:
         # submit edit group
         wd.find_element_by_name("edit").click()
         self.fill_form(group)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
 
     def return_to_groups_page(self):
         wd = self.app.wd
---
+++ test/test_modify_group.py
@@ -0,0 +1,11 @@
+from model.group import Group
+
+def test_modify_first_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="New group name"))
+    app.session.logout()
+
+def test_modify_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New group header"))
+    app.session.logout()
