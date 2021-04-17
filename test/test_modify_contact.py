d7ed345c95591dee28eb777d45525ffaae702101
GatskevichVika <gatskevich.vika@gmail.com>
2021-04-17 11:16:38 +0300

    Обеспечила выполнение предусловий в тестах для удаления и модификации для групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,10 +2,19 @@
 from model.contact import Contact
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(
+            Contact(firstname="test", middlename="test", lastname="test", company="test", mobile="test", email="test"))
     app.contact.modify_first_contact(Contact(firstname="Николай"))
 
 def test_modify_contact_middlename(app):
+    if app.contact.count() == 0:
+        app.contact.create(
+            Contact(firstname="test", middlename="test", lastname="test", company="test", mobile="test", email="test"))
     app.contact.modify_first_contact(Contact(middlename="Николаевич"))
 
 def test_modify_contact_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create(
+            Contact(firstname="test", middlename="test", lastname="test", company="test", mobile="test", email="test"))
     app.contact.modify_first_contact(Contact(lastname="Николаев"))
