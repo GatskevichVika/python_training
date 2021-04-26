738c4c8c420c5080e233fbc1898919f699b30cd6
GatskevichVika <gatskevich.vika@gmail.com>
2021-04-26 10:14:32 +0300

    Реализовала проверки в виде сравнения списков для групп и контактов.

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -4,7 +4,6 @@ def test_edit_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test", lastname="test"))
     old_contacts = app.contact.get_contact_list
-    app.contact.edit(Contact(firstname="Иван", middlename="Иванович", lastname="Иванов", company="Иванстрой",
-                             mobile="8888888", email="ivan@ivan.ru"))
+    app.contact.edit(Contact(firstname="Иван", lastname="Иванов"))
     new_contacts = app.contact.get_contact_list
     assert len(old_contacts) == len(new_contacts)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,11 +5,16 @@ def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
         app.contact.create(
             Contact(firstname="test", lastname="test"))
-    old_contacts = app.contact.get_contact_list
-    contact = Contact(lastname="Николаев")
+
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(lastname="Николаев", firstname="Николай")
+
     contact.id = old_contacts[0].id
+    contact.firstname = old_contacts[0].firstname
+    contact.lastname = old_contacts[0].lastname
+
     app.contact.modify_first_contact(contact)
-    new_contacts = app.contact.get_contact_list
+    new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
