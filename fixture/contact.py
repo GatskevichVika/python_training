91a9814559f0f7333e44c4ac2318354c0da2f0ae
GatskevichVika <gatskevich.vika@gmail.com>
2021-04-28 22:54:54 +0300

    Реализовала проверки для всех полей контакта на главной странице

--- fixture/contact.py
+++ fixture/contact.py
@@ -42,9 +42,10 @@ class ContactHelper:
         self.change_field_value("mobile", contact.mobile)
         self.change_field_value("work", contact.work)
         self.change_field_value("phone2", contact.phone2)
+        self.change_field_value("email", contact.email)
         self.change_field_value("email2", contact.email2)
         self.change_field_value("email3", contact.email3)
-        self.change_field_value("email", contact.email)
+
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -102,10 +103,13 @@ class ContactHelper:
                 cells = row.find_elements_by_tag_name("td")
                 lastname = cells[1].text
                 firstname = cells[2].text
+                address = cells[3].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                all_phones = cells[5].text
-                self.contact_cache.append(Contact(lastname=lastname, firstname=firstname, id=id,
-                                                  all_phones_from_home_page=all_phones))
+                contact_and_email = (cells[5].text + "\n" + cells[4].text)
+                #all_emails = cells[4].text
+                #all_phones = cells[5].text
+                self.contact_cache.append(Contact(lastname=lastname, firstname=firstname, id=id, address=address,
+                                                  contact_and_email=contact_and_email))
         return list(self.contact_cache)
 
     def select_contact_by_index(self, index):
@@ -133,12 +137,19 @@ class ContactHelper:
         firstname = wd.find_element_by_name("firstname").get_attribute("value")
         lastname = wd.find_element_by_name("lastname").get_attribute("value")
         id = wd.find_element_by_name("id").get_attribute("value")
-        homephone = wd.find_element_by_name("home").get_attribute("value")
-        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
-        workphone = wd.find_element_by_name("work").get_attribute("value")
-        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
-        return Contact(firstname=firstname, lastname=lastname, id=id, home=homephone, mobile=mobilephone,
-                       work=workphone, phone2=secondaryphone)
+        address = wd.find_element_by_name("address").get_attribute("value")
+
+        home = wd.find_element_by_name("home").get_attribute("value")
+        mobile = wd.find_element_by_name("mobile").get_attribute("value")
+        work = wd.find_element_by_name("work").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
+
+        return Contact(firstname=firstname, lastname=lastname, id=id, address=address, home=home,
+                       mobile=mobile, work=work, phone2=phone2, email=email, email2=email2, email3=email3)
 
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
@@ -150,10 +161,3 @@ class ContactHelper:
         secondaryphone = re.search("P: (.*)", text).group(1)
         return Contact(home=homephone, mobile=mobilephone,
                        work=workphone, phone2=secondaryphone)
-
-
-
-
-
-
-
--- model/contact.py
+++ model/contact.py
@@ -3,8 +3,8 @@ from sys import maxsize
 class Contact:
 
     def __init__(self, firstname=None, middlename=None, address=None, lastname=None, company=None, home=None,
-                 mobile=None, work=None, phone2=None, email=None, id=None,
-                 all_phones_from_home_page=None, email2=None, email3=None):
+                 mobile=None, work=None, phone2=None, email=None, id=None, contact_and_email=None,
+                 all_phones_from_home_page=None, email2=None, email3=None, all_emails_from_home_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.address = address
@@ -19,6 +19,8 @@ class Contact:
         self.all_phones_from_home_page = all_phones_from_home_page
         self.email2 = email2
         self.email3 = email3
+        self.all_phones_from_home_page = all_emails_from_home_page
+        self.contact_and_email = contact_and_email
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
