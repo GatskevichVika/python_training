bca3e27826b65015d48c86bbf37bc97501ffc072
GatskevichVika <gatskevich.vika@gmail.com>
2021-05-17 18:20:37 +0300

    Добавила ORM

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,7 +1,7 @@
 from model.contact import Contact
 import random
 
-def test_delete_some_contact(app, db):
+def test_delete_some_contact(app, db, check_ui):
 
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="test", lastname="test"))
@@ -13,4 +13,8 @@ def test_delete_some_contact(app, db):
     new_contacts = db.get_contact_list()
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max()) == sorted(app.group.get_contact_list(),
+                                                                       key=Contact.id_or_max())
+
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -11,9 +11,6 @@ def test_modify_contact(app):
     contact = Contact(lastname="Николаев", firstname="Николай", middlename="Николаевич")
     index = randrange(len(old_contacts))
     contact.id = old_contacts[index].id
-    #contact.firstname = old_contacts[index].firstname
-    #contact.lastname = old_contacts[index].lastname
-
     app.contact.modify_contact_by_index(index, contact)
 
     assert len(old_contacts) == app.contact.count()
@@ -21,21 +18,3 @@ def test_modify_contact(app):
 
     old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-#def test_modify_contact_middlename(app):
-#    if app.contact.count() == 0:
-#        app.contact.create(
-#            Contact(firstname="test",  lastname="test"))
-#    old_contacts = app.contact.get_contact_list()
-#    app.contact.modify_first_contact(Contact(middlename="Николаевич"))
-#    new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) == len(new_contacts)
-
-#def test_modify_contact_lastname(app):
-#    if app.contact.count() == 0:
-#        app.contact.create(
- #           Contact(firstname="test", lastname="test"))
-#    old_contacts = app.contact.get_contact_list()
-#    app.contact.modify_first_contact(Contact(lastname="Николаев"))
-#    new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) == len(new_contacts)
