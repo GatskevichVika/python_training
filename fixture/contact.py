54835f0db80aab41c3a3d649784654408b52b88b
GatskevichVika <gatskevich.vika@gmail.com>
2021-05-17 13:19:59 +0300

    Доработала тест удаления контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -79,6 +79,17 @@ class ContactHelper:
         wd.find_element_by_css_selector("div.msgbox")
         self.contact_cache = None
 
+    def delete_contact_by_id(self, id):
+        wd = self.app.wd
+        self.open_contact_page()
+        self.select_contact_by_id(id)
+        # submit deletion
+        wd.find_element_by_xpath('//input[@value="Delete"]').click()
+
+        wd.switch_to_alert().accept()
+        wd.find_element_by_css_selector("div.msgbox")
+        self.contact_cache = None
+
     def select_first_contact(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
@@ -115,6 +126,10 @@ class ContactHelper:
     def select_contact_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_contact_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
     def open_contact_to_edit_by_index(self, index):
         wd = self.app.wd
