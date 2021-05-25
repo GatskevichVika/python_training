69aab02fa000f6d032bae13c11c3f6c15f8280e9
GatskevichVika <gatskevich.vika@gmail.com>
2021-05-25 11:16:35 +0300

    Задание 21

--- test/test_all_contact.py
+++ test/test_all_contact.py
@@ -1,13 +1,38 @@
+import re
+import random
 
 from model.contact import Contact
 
 def test_contact_from_home_page(app, db):
     # получить список всех контаков на главной
-    contact_from_home_page = app.contact.get_contact_list()
-
+    contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
     # получить список всех контактов из БД
-    contact_from_bd = db.get_contact_list()
-    # сравнить списки
-    assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_bd, key=Contact.id_or_max)
+    contact_from_bd = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    index = 0
+    for row in contact_from_home_page:
+        assert contact_from_home_page[index] == contact_from_bd[index]
+        #assert contact_from_home_page[index].lastname == contact_from_bd[index].lastname
+        #assert contact_from_home_page[index].firstname == contact_from_bd[index].firstname
+        #assert contact_from_home_page[index].address == contact_from_bd[index].address
+        #assert contact_from_home_page[index].all_phones_from_home_page == merge_phones_like_from_home_page(
+            #contact_from_bd[index])
+        #assert contact_from_home_page[index].all_emails_from_home_page == merge_emails_like_from_home_page(
+            #contact_from_bd[index])
+    index = index + 1
 
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_phones_like_from_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                      [contact.home, contact.mobile, contact.work, contact.phone2]))))
+
+def merge_emails_like_from_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                      [contact.email, contact.email2, contact.email3]))))
 
