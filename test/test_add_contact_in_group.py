24fb4b358ea5f29005924fb393e3d29d8247fb8e
GatskevichVika <gatskevich.vika@gmail.com>
2021-06-30 11:00:39 +0300

    Задание 22 добавила ещё один запрос к БД

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -8,16 +8,17 @@ db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 def test_add_contact_in_group(app):
 
     # получить список контактов, если контактов нет создать контакт
-    contact_list = db.get_contact_list()
-    if len(contact_list) == 0:
+    old_contact_list = db.get_contact_list()
+    if len(old_contact_list) == 0:
         app.contact.create(Contact(firstname="test"))
     # получить список групп, если групп нет создать группу
-    group_list = db.get_group_list()
-    if len(group_list) == 0:
+    old_group_list = db.get_group_list()
+    if len(old_group_list) == 0:
         app.group.create(Group(name="test"))
-
+    new_contact_list = db.get_contact_list()
+    new_group_list = db.get_group_list()
     # выбираем случайную группу из списка
-    group = random.choice(group_list)
+    group = random.choice(new_group_list)
     # Проверяем какие контакты в нее не входят
     contact_not_in_group = db.get_contact_not_in_group(Group(id=group.id))
     # выбрать контакт который надо добавить в группу
